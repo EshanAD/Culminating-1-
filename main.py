667fc54c88012f2dea87a3bb193d0cbdca3ea3dd
Eshan <121054480+EshanAD@users.noreply.github.com>
2023-01-14 05:07:46 +0000

    The new code I added yesterday that was not prevously created that did not save starts on line 548 in BlackJack.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Name: Eshan Adatia 
-#Date: December 20, 2022
+#Date: January 20, 2022
 #Progam Name: BlackJack Game with GUI and Database
 #Description: BlackJack game that has a login in the window using a database to determine the funds in which the user can use/bet. The game is shown through GUI using buttons 
 #Program Purpose: Create a functional game that incorporates everything we learned in grade 11 comp sci
